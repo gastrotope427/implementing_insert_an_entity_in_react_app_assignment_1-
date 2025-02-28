b7c1d25bff7d547ff1a260597efa2950e131848e
gastrotope427 <gaurang.mishra.s64@kalvium.community>
2025-02-28 11:54:45 +0530

    done

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,18 +1,15 @@
-// src/components/Home.jsx
-
 import React from "react";
-import BookCard from "./components/BookCard";
-import books from "./booksData";
-import "./Home.css"; // Import the CSS for styling
+import { Link } from "react-router-dom";
 
 const Home = () => {
   return (
-    <div className="home-container">
+    <div>
       <h1>Book Library</h1>
-      <div className="books-grid">
-        {books.map((book) => (
-          <BookCard key={book.id} book={book} />
-        ))}
+      <Link to="/add-book">
+        <button style={{ padding: "10px", fontSize: "16px" }}>Add Book</button>
+      </Link>
+      <div className="book-list">
+        {/* Render BookCard components here */}
       </div>
     </div>
   );
